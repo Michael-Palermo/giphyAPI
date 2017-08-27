6ac49bab388c6fa9a3d3f605accbdaa183789a68
map88az <micpalermo@gmail.com>
2017-08-26 20:43:11 -0700

    final

---
+++ assets/javascript/giphyapi.js
@@ -0,0 +1,60 @@
+ var buttons = ["penguin", "ponyo", "dolphin", "Amber Rose", "lion", "cheetah", "tiger"];
+
+function renderButtons() {
+  $("#buttons-view").empty();
+  for (var i = 0; i < buttons.length; i++) {
+    var a = $("<button class='btn btn-primary'>");
+    a.addClass("searches");
+    a.attr("data-search", buttons[i]);
+    a.text(buttons[i]);
+    $("#buttons-view").append(a);
+  }
+}
+
+function displayGifInfo() {
+  var queryTerm = $(this).attr("data-search");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=4f5aa4196409493995940a627c425a76&q=" + queryTerm + "&limit=10&offset=0&rating=PG&lang=en"
+  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    console.log(response);
+    for (var i = 0; i < response.data.length; i++) {
+      var imageUrl = response.data[i].images.fixed_height_still.url;
+      var newImage = $("<img>");
+      newImage.addClass("imageSearch");
+      newImage.attr("src", imageUrl);
+      newImage.attr("data-state", "still");
+      newImage.attr("image-still", imageUrl);
+      newImage.attr("image-animate", response.data[i].images.fixed_height.url);
+      $("#image-dump").prepend("<span class='ratingSpan'> Rating: " + response.data[i].rating + "</span>");
+      $("#image-dump").prepend(newImage);
+    }
+  });
+  $("#image-dump").empty();
+}
+
+renderButtons();
+
+$("#add-term").on("click", function(event) {
+  event.preventDefault();
+  var term = $("#search-input").val().trim();
+  buttons.push(term);
+  renderButtons();
+  $("#search-input").val("");
+});
+
+$(document).on("click", ".searches", displayGifInfo);
+
+$(document).on("click", ".imageSearch", function animateGif() {
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("image-animate"));
+    $(this).attr("data-state", "animate");
+  }
+  else if (state === "animate") {
+    $(this).attr("src", $(this).attr("image-still"));
+    $(this).attr("data-state", "still");
+  }
+});
